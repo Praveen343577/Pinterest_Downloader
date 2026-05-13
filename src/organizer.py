d7441299efc2cedd9bf5dd59ab34ece63edea0bc
Praveen343577 <0praveen1ahirwar22@gmail.com>
2026-05-13 13:21:30 +0530

    fixed account username error

--- src/organizer.py
+++ src/organizer.py
@@ -1,20 +1,51 @@
 import os
 import shutil
+import json
 import config
+
+def extract_account_name(data):
+    for key in ['native_creator', 'pinner', 'origin_pinner', 'owner']:
+        if key in data and data[key] and 'username' in data[key]:
+            return data[key]['username']
+    if 'board' in data and data['board'] and 'owner' in data['board'] and 'username' in data['board']['owner']:
+        return data['board']['owner']['username']
+    return "unknown_user"
 
 def organize_metadata():
     for filename in os.listdir(config.OUTPUT_BASE):
         if filename.endswith(".json"):
-            src = os.path.join(config.OUTPUT_BASE, filename)
-            if not os.path.isfile(src):
+            json_path = os.path.join(config.OUTPUT_BASE, filename)
+            if not os.path.isfile(json_path):
                 continue
                 
-            base_name, ext = os.path.splitext(filename)
-            dst_filename = filename
-            counter = 1
+            media_filename = filename[:-5]
+            media_path = os.path.join(config.OUTPUT_BASE, media_filename)
             
-            while os.path.exists(os.path.join(config.METADATA_DIR, dst_filename)):
-                dst_filename = f"{base_name}_{counter}{ext}"
-                counter += 1
+            with open(json_path, 'r', encoding='utf-8') as f:
+                try:
+                    data = json.load(f)
+                except json.JSONDecodeError:
+                    continue
+                    
+            account_name = extract_account_name(data)
+            ext = media_filename.split('.')[-1].lower() if '.' in media_filename else 'unknown'
+            type_prefix = 'v' if ext in ['mp4', 'webm', 'mov', 'm4v', 'avi'] else 'p'
+            
+            seq_num = 1
+            new_base_name = f"{account_name} {type_prefix}{seq_num}"
+            new_media_filename = f"{new_base_name}.{ext}"
+            new_media_path = os.path.join(config.OUTPUT_BASE, new_media_filename)
+            
+            while os.path.exists(new_media_path):
+                seq_num += 1
+                new_base_name = f"{account_name} {type_prefix}{seq_num}"
+                new_media_filename = f"{new_base_name}.{ext}"
+                new_media_path = os.path.join(config.OUTPUT_BASE, new_media_filename)
                 
-            shutil.move(src, os.path.join(config.METADATA_DIR, dst_filename))
+            new_json_filename = f"{new_base_name}.{ext}.json"
+            
+            if os.path.exists(media_path):
+                os.rename(media_path, new_media_path)
+                
+            dst_json_path = os.path.join(config.METADATA_DIR, new_json_filename)
+            shutil.move(json_path, dst_json_path)
